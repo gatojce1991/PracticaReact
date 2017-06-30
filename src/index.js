51a889d62621bd078a3beb4a628980af888d1488
gatojce1991 <gatojce@gmail.com>
2017-06-29 20:10:42 -0500

    Segundo commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const PriComponente = () => (
-<div className="text-center">
-    hola mundo
-</div>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
-ReactDOM.render(<PriComponente />, document.getElementById('root'));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// const PriComponente = () => (
+// <div className="text-center">
+//     hola mundo
+// </div>
+// );
+
+// ReactDOM.render(<PriComponente />, document.getElementById('root'));
